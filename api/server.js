85d70c194c678ea06ea40ddce93182076ffa5a98
Walter Prince <wprince0288@gmail.com>
2025-04-22 20:43:30 -0500

    basic server built

--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
 // build your server here and require it from index.js
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.json({ message: 'Server is running!' });
+});
+
+module.exports = server;
