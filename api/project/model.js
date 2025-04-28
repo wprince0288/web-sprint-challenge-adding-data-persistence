f6f46475e316917096f9f035761d3f098c1cb2d9
Walter Prince <wprince0288@gmail.com>
2025-04-27 20:32:09 -0500

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,23 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+async function getAll() {
+    const projects = await db('projects')
+    return projects.map(p => ({
+        ...p,
+        project_completed: !!p.project_completed
+    }))
+}
+
+async function create(project) {
+    try {
+
+        const [id] = await db('projects').insert(project, 'project_id')
+        const newProject = await db('projects').where('project_id', id).first()
+        return { ...newProject, project_completed: Boolean(newProject.project_completed) }
+    } catch (err) {
+        throw new Error('Failed to create project: ' + err.message)
+    }
+}
+
+module.exports = { getAll, create }
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,29 @@
 // build your `/api/projects` router here
+const express = require('express')
+const Projects = require('/model')
+
+const router = express.Router()
+
+router.get("/", async (req, res, next) => {
+    try {
+        const projects = await Projects.getAll()
+        res.json(projects)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+        const { project_name } = req.body
+        if (!project_name) {
+            return res.status(400).json({ message: "Project name is required" })
+        }
+        const newProject = await Projects.create(req.body)
+        res.status(201).json(newProject)
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
