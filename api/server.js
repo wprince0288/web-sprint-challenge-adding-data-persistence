254774672ee4470aaedcb6b3005c1cd0026198aa
Walter Prince <wprince0288@gmail.com>
2025-04-28 21:15:38 -0500

    wipp

--- api/server.js
+++ api/server.js
@@ -1,32 +1,32 @@
 // build your server here and require it from index.js
 const express = require('express');
 
+const tasksRouter = require('./api/task/router');
+const resourcesRouter = require('./api/resource/router');
+const projectsRouter = require('./api/project/router');
+
 const server = express();
+
+server.use(express.json());
+
+server.use('/api/task', tasksRouter);
+server.use('/api/resource', resourcesRouter);
+server.use('/api/project', projectsRouter);
 
 server.get('/', (req, res) => {
     res.json({ message: 'Server is running!' });
 });
 
+server.use('*', (req, res) => {
+    res.status(404).json({ message: 'Not found' });
+});
+
+server.use((err, req, res, next) => {
+    console.error(err);
+    res.status(err.status || 500).json({
+        message: err.message || 'Internal server error',
+        stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
+    });
+});
+
 module.exports = server;
-
-
-// const express = require('express')
-
-// const projectRouter = require('./project/router.js')
-// const resourceRouter = require('./resource/router.js')
-// const taskRouter = require('./task/router.js')
-
-// const server = express()
-
-// server.use(express.json())
-
-// server.use("/api/projects", projectRouter)
-// server.use("/api/resources", resourceRouter)
-// server.use("/api/tasks", taskRouter)
-
-// //eslint-disable-next-line
-// server.use((err, req, res, next) => { 
-//     res.status(500).json({ message: "Something went wrong", error: err.message})
-// })
-
-// module.exports = server
--- api/task/model.js
+++ api/task/model.js
@@ -1,7 +1,7 @@
 // build your `Task` model here
 //test//
 
-const db = require('../../data/dbConfig')
+const db = require('../../data/dbConfig');
 
 async function getAll() {
     const tasks = await db('tasks as t')
@@ -13,18 +13,29 @@ async function getAll() {
             't.task_completed',
             'p.project_name',
             'p.project_description'
-        )
+        );
 
     return tasks.map(t => ({
         ...t,
         task_completed: Boolean(t.task_completed)
-    }))
+    }));
 }
 
 async function create(task) {
-    const [id] = await db('tasks').insert(task)
-    const newTask = await db('tasks').where('task_id', id).first()
-    return {...newTask, task_completed: Boolean(newTask.task_completed)}
+    const [id] = await db('tasks').insert(task);
+    const newTask = await db('tasks as t')
+        .join('projects as p', 't.project_id', 'p.project_id')
+        .select(
+            't.task_id',
+            't.task_description',
+            't.task_notes',
+            't.task_completed',
+            'p.project_name',
+            'p.project_description'
+        ).where('task_id', id)
+        .first();
+
+    return { ...newTask, task_completed: Boolean(newTask.task_completed) };
 }
 
-module.exports = { getAll, create }
+module.exports = { getAll, create };
